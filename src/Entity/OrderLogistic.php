3b8b2b5a7d4993e32d428ef1faeb506f54d71a4c
Luiz Kim <luiz.kim@controleonline.com>
2024-01-08 21:03:26 -0300

    Update

--- src/Entity/OrderLogistic.php
+++ src/Entity/OrderLogistic.php
@@ -41,8 +41,6 @@ use App\Filter\OrderLogisticEntityFilter;
     denormalizationContext: ['groups' => ['logistic_write']])]
 #[ApiFilter(filterClass: SearchFilter::class, properties: ['shippingDate' => 'exact', 'arrivalDate' => 'exact', 'order.id' => 'partial', 'order.contract.id' => 'partial', 'order.client.name' => 'partial', 'order.productType' => 'partial', 'order.otherInformations' => 'partial', 'provider' => 'exact', 'destinationProvider' => 'exact', 'status' => 'exact', 'originAddress' => 'partial'])]
 
-#[ApiFilter(CustomOrFilter::class, properties:['originCity', 'originState'])]
-
 class OrderLogistic
 {
     /**

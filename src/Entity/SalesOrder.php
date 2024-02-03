743129ca458dd15a6d48dd1f9c96051cd853080b
Luiz Kim <luiz.kim@controleonline.com>
2024-02-03 18:47:42 -0300

    Update

--- src/Entity/SalesOrder.php
+++ src/Entity/SalesOrder.php
@@ -1499,4 +1499,48 @@ class SalesOrder extends Order
     {
         return $this->orderQueue;
     }
+
+    public function isOriginAndDestinationTheSame(): ?bool
+    {
+        if (($origin = $this->getAddressOrigin()) === null) {
+            return null;
+        }
+
+        if (($destination = $this->getAddressDestination()) === null) {
+            return null;
+        }
+
+        $origCity = $origin->getStreet()->getDistrict()->getCity();
+        $destCity = $destination->getStreet()->getDistrict()->getCity();
+
+        // both objects are the same entity ( = same name and same state)
+
+        if ($origCity === $destCity) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isOriginAndDestinationTheSameState(): ?bool
+    {
+        if (($origin = $this->getAddressOrigin()) === null) {
+            return null;
+        }
+
+        if (($destination = $this->getAddressDestination()) === null) {
+            return null;
+        }
+
+        $origState = $origin->getStreet()->getDistrict()->getCity()->getState();
+        $destState = $destination->getStreet()->getDistrict()->getCity()->getState();
+
+        // both objects are the same entity ( = same name and same country)
+
+        if ($origState === $destState) {
+            return true;
+        }
+
+        return false;
+    }
 }

e941d17df81ceee80308d2a297f17bdda70439cd
Luiz Kim <luiz.kim@controleonline.com>
2024-02-03 17:40:45 -0300

    Update

--- src/Entity/SalesOrder.php
+++ src/Entity/SalesOrder.php
@@ -170,7 +170,7 @@ use stdClass;
  * )
  */
 
-class SalesOrder extends Order
+class SalesOrder
 {
     /**
      * @var integer
@@ -1498,5 +1498,49 @@ class SalesOrder extends Order
     public function getOrderQueue()
     {
         return $this->orderQueue;
+    }
+
+    public function isOriginAndDestinationTheSame(): ?bool
+    {
+        if (($origin = $this->getAddressOrigin()) === null) {
+            return null;
+        }
+
+        if (($destination = $this->getAddressDestination()) === null) {
+            return null;
+        }
+
+        $origCity = $origin->getStreet()->getDistrict()->getCity();
+        $destCity = $destination->getStreet()->getDistrict()->getCity();
+
+        // both objects are the same entity ( = same name and same state)
+
+        if ($origCity === $destCity) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isOriginAndDestinationTheSameState(): ?bool
+    {
+        if (($origin = $this->getAddressOrigin()) === null) {
+            return null;
+        }
+
+        if (($destination = $this->getAddressDestination()) === null) {
+            return null;
+        }
+
+        $origState = $origin->getStreet()->getDistrict()->getCity()->getState();
+        $destState = $destination->getStreet()->getDistrict()->getCity()->getState();
+
+        // both objects are the same entity ( = same name and same country)
+
+        if ($origState === $destState) {
+            return true;
+        }
+
+        return false;
     }
 }

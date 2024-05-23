65a16a8dc6cbfcc988712504355b05b6f03a8b64
Luiz Kim <luiz.kim@controleonline.com>
2024-05-22 22:54:01 -0300

    Update

---
+++ src/Service/OrderProductService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ControleOnline\Service;
+
+use ControleOnline\Entity\OrderProduct;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class OrderProductService
+{
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private Security $security
+    ) {
+    }
+
+    public function afterPersist(OrderProduct $OrderProduct)
+    {
+        $order = $OrderProduct->getOrder();
+
+        $sql = 'SELECT SUM(price * quantity) as total FROM order_product WHERE order_id = :order_id';
+        $connection = $this->manager->getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute(['order_id' =>  $order->getId()]);
+
+        $result = $statement->fetchOne();
+        $order->setPrice($result['total']);
+        $this->manager->persist($order);
+        $this->manager->flush();
+    }
+}

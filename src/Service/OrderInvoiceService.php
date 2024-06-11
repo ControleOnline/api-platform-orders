1e98642940dc062ad6b9235286da6d7c3d9b966b
Luiz Kim <luiz.kim@controleonline.com>
2024-06-11 12:02:43 -0300

    Update Invoices

---
+++ src/Service/OrderInvoiceService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ControleOnline\Service;
+
+use ControleOnline\Entity\OrderInvoice;
+use ControleOnline\Entity\OrderProduct;
+use ControleOnline\Entity\Status;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class OrderInvoiceService
+{
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private Security $security
+    ) {
+    }
+
+    public function afterPersist(OrderInvoice $OrderInvoice)
+    {
+        $invoice = $OrderInvoice->getInvoice();
+        $order = $OrderInvoice->getOrder();
+        if ($invoice->getStatus()->getStatus() == 'Pago') {
+            $orderStatus = $this->manager->getRepository(Status::class)->findOneBy([
+                'status' => 'Aguardando Pagamento',
+                'context' => 'order'
+            ]);
+            $order->setStatus($orderStatus);
+            $this->manager->persist($order);
+            $this->manager->flush();
+        }
+        return $order;
+    }
+}

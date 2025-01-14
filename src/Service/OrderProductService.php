1846e8c1c93e85e1555d38d865354fbcefcc9b33
Luiz Kim <luiz.kim@controleonline.com>
2025-01-14 17:14:32 -0300

    update

--- src/Service/OrderProductService.php
+++ src/Service/OrderProductService.php
@@ -5,12 +5,14 @@ namespace ControleOnline\Service;
 use ControleOnline\Entity\OrderProduct;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\QueryBuilder;
 
 class OrderProductService
 {
     public function __construct(
         private EntityManagerInterface $manager,
-        private Security $security
+        private Security $security,
+        private PeopleService $PeopleService
     ) {
     }
 
@@ -28,5 +30,13 @@ class OrderProductService
         $this->manager->persist($order);
         $this->manager->flush();
         return $order;
+    }
+
+    private function  secutiryFilter(QueryBuilder $queryBuilder, $resourceClass = null, $applyTo = null, $rootAlias = null): void
+    {
+      //$queryBuilder->join(sprintf('%s.order', $rootAlias), 'o');
+      //$queryBuilder->andWhere('o.client IN(:companies) OR o.provider IN(:companies)');
+      //$companies   = $this->PeopleService->getMyCompanies();
+      //$queryBuilder->setParameter('companies', $companies);
     }
 }

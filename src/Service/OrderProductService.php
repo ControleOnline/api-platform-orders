19cb68a3331164a482d59a555d2bfa7482e58d70
Luiz Kim <luiz.kim@controleonline.com>
2025-02-03 19:17:38 -0300

    Update

--- src/Service/OrderProductService.php
+++ src/Service/OrderProductService.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\QueryBuilder;
 
 class OrderProductService
 {
-    private $orderProduct;
 
     public function __construct(
         private EntityManagerInterface $manager,
@@ -20,28 +19,33 @@ class OrderProductService
 
     public function afterPersist(OrderProduct $orderProduct)
     {
-        $this->calculateProductPrice($orderProduct);
-        return $orderProduct;
+        return $this->calculateProductPrice($orderProduct);
     }
 
     public function beforeDelete(OrderProduct $orderProduct)
     {
-        $this->orderProduct = $orderProduct;
         $parentProducts = $this->manager->getRepository(OrderProduct::class)->findBy([
             'parentProduct'  => $orderProduct->getProduct(),
         ]);
         foreach ($parentProducts as $parentProduct)
             $this->manager->remove($parentProduct);
         $this->manager->flush();
+
+        return $this->calculateProductPrice($orderProduct);
     }
 
     private function calculateProductPrice(OrderProduct $orderProduct)
     {
-        $this->orderProduct = $orderProduct;
+
         $orderProduct->setPrice($orderProduct->getProduct()->getPrice());
         $orderProduct->setTotal($orderProduct->getPrice() * $orderProduct->getQuantity());
         $this->manager->persist($orderProduct);
         $this->manager->flush();
+
+        $this->orderService->calculateGroupProductPrice($orderProduct->getOrder());
+        $this->orderService->calculateOrderPrice($orderProduct->getOrder());
+
+        $this->manager->refresh($orderProduct);
 
         return $orderProduct;
     }
@@ -54,15 +58,5 @@ class OrderProductService
         //$queryBuilder->andWhere('o.client IN(:companies) OR o.provider IN(:companies)');
         //$companies   = $this->peopleService->getMyCompanies();
         //$queryBuilder->setParameter('companies', $companies);
-    }
-
-
-    public function __destruct()
-    {
-        if (!$this->orderProduct)
-            return;
-
-        $this->orderService->calculateGroupProductPrice($this->orderProduct->getOrder());
-        $this->orderService->calculateOrderPrice($this->orderProduct->getOrder());
     }
 }

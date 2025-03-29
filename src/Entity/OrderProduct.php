f112848785f829b312949ecef69f73b5b0e9cfd2
Luiz Kim <luiz.kim@controleonline.com>
2025-03-29 19:24:15 -0300

    Update

--- src/Entity/OrderProduct.php
+++ src/Entity/OrderProduct.php
@@ -2,6 +2,7 @@
 
 namespace ControleOnline\Entity;
 
+
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use ApiPlatform\Core\Annotation\ApiSubresource;
@@ -9,6 +10,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use stdClass;
 use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Doctrine\Orm\Filter\ExistsFilter;
 use ApiPlatform\Metadata\GetCollection;
@@ -17,20 +19,33 @@ use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiProperty;
 
 /**
- * OrderProduct
+ *  OrderProduct
  *
+ * @ORM\EntityListeners({ControleOnline\Listener\LogListener::class})
  * @ORM\Table(name="order_product")
  * @ORM\Entity(repositoryClass="ControleOnline\Repository\OrderProductRepository")
  */
+
 #[ApiResource(
     operations: [
-        new Get(security: 'is_granted(\'ROLE_CLIENT\')'),
-        new GetCollection(security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'ROLE_CLIENT\')'),
-        new Post(security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'ROLE_CLIENT\')'),
-        new Put(security: 'is_granted(\'ROLE_ADMIN\') or (is_granted(\'ROLE_CLIENT\'))'),
-        new Delete(security: 'is_granted(\'ROLE_ADMIN\') or (is_granted(\'ROLE_CLIENT\'))'),
+        new Get(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+        ),
+        new GetCollection(
+            security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'ROLE_CLIENT\')',
+        ),
+        new Post(
+            security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'ROLE_CLIENT\')',
+        ),
+        new Put(
+            security: 'is_granted(\'ROLE_ADMIN\') or (is_granted(\'ROLE_CLIENT\'))',
+        ),
+        new Delete(
+            security: 'is_granted(\'ROLE_ADMIN\') or (is_granted(\'ROLE_CLIENT\'))',
+        ),
     ],
     formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
     normalizationContext: ['groups' => ['order_product:read']],
@@ -38,6 +53,7 @@ use ApiPlatform\Metadata\ApiResource;
 )]
 #[ApiFilter(filterClass: OrderFilter::class, properties: ['alterDate' => 'DESC'])]
 #[ApiFilter(OrderFilter::class, properties: ['id' => 'ASC', 'product.product' => 'ASC'])]
+
 class OrderProduct
 {
     /**
@@ -65,6 +81,7 @@ class OrderProduct
     #[ApiFilter(filterClass: SearchFilter::class, properties: ['product' => 'exact'])]
     #[ApiFilter(filterClass: SearchFilter::class, properties: ['product.type' => 'exact'])]
     private $product;
+
 
     /**
      * @ORM\ManyToOne(targetEntity="ControleOnline\Entity\Product")
@@ -96,6 +113,7 @@ class OrderProduct
     #[ApiFilter(filterClass: SearchFilter::class, properties: ['productGroup.type' => 'exact'])]
     private $productGroup;
 
+
     /**
      * @ORM\Column(type="float")
      * @Groups({"order_product_queue:read","order:read","order_details:read","order:write","order_product:write","order_product:read"})
@@ -116,102 +134,165 @@ class OrderProduct
 
     // Getters and setters
 
+    /**
+     * Get the value of id
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Set the value of id
+     */
     public function setId($id): self
     {
         $this->id = $id;
+
         return $this;
     }
 
+    /**
+     * Get the value of order
+     */
     public function getOrder()
     {
         return $this->order;
     }
 
+    /**
+     * Set the value of order
+     */
     public function setOrder($order): self
     {
         $this->order = $order;
+
         return $this;
     }
 
+    /**
+     * Get the value of product
+     */
     public function getProduct()
     {
         return $this->product;
     }
 
+    /**
+     * Set the value of product
+     */
     public function setProduct($product): self
     {
         $this->product = $product;
+
         return $this;
     }
 
+    /**
+     * Get the value of quantity
+     */
     public function getQuantity()
     {
         return $this->quantity;
     }
 
+    /**
+     * Set the value of quantity
+     */
     public function setQuantity($quantity): self
     {
         $this->quantity = $quantity;
+
         return $this;
     }
 
+    /**
+     * Get the value of price
+     */
     public function getPrice()
     {
         return $this->price;
     }
 
+    /**
+     * Set the value of price
+     */
     public function setPrice($price): self
     {
         $this->price = $price;
+
         return $this;
     }
 
+    /**
+     * Get the value of total
+     */
     public function getTotal()
     {
         return $this->total;
     }
 
+    /**
+     * Set the value of total
+     */
     public function setTotal($total): self
     {
         $this->total = $total;
+
         return $this;
     }
 
+    /**
+     * Get the value of parentProduct
+     */
     public function getParentProduct()
     {
         return $this->parentProduct;
     }
 
+    /**
+     * Set the value of parentProduct
+     */
     public function setParentProduct($parentProduct): self
     {
         $this->parentProduct = $parentProduct;
+
         return $this;
     }
 
+    /**
+     * Get the value of orderProduct
+     */
     public function getOrderProduct()
     {
         return $this->orderProduct;
     }
 
+    /**
+     * Set the value of orderProduct
+     */
     public function setOrderProduct($orderProduct): self
     {
         $this->orderProduct = $orderProduct;
+
         return $this;
     }
 
+    /**
+     * Get the value of productGroup
+     */
     public function getProductGroup()
     {
         return $this->productGroup;
     }
 
+    /**
+     * Set the value of productGroup
+     */
     public function setProductGroup($productGroup): self
     {
         $this->productGroup = $productGroup;
+
         return $this;
     }
 }
